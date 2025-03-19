32a4e758d96f5236d972d4cc0e6758d0769d7375
kabilanM <kabilan200207@gamil.com>
2025-03-20 05:04:00 +0530

    intial commit to git repo

---
+++ Tem project/scripts.js
@@ -0,0 +1,13 @@
+
+  var swiper = new Swiper('.swiper-container', {
+
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
